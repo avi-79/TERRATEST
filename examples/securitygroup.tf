a37b154ebf688e8354e09995a7806614412e5315
root <root@ip-172-31-3-35.us-east-2.compute.internal>
2022-05-25 18:20:03 +0000

    adding files

---
+++ examples/securitygroup.tf
@@ -0,0 +1,8 @@
+resource "aws_security_group" "instance" {
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
---
+++ examples/var.tf
@@ -0,0 +1,16 @@
+variable "AWS_ACCESS_KEY" {
+}
+
+variable "AWS_SECRET_KEY" {
+}
+
+variable "AWS_REGION" {
+  default = "us-east-2"
+}
+
+variable "AMIS" {
+  type = map(string)
+  default = {
+    ap-south-1 = "ami-0fa49cc9dc8d62c84"
+  }
+}
