a3f4839c84cf73c57f8ef5d9e3d7d057b1990ab8
avi-79 <92776072+avi-79@users.noreply.github.com>
2022-06-07 10:46:49 +0530

    Update var.tf

--- examples/var.tf
+++ examples/var.tf
@@ -1,16 +1,12 @@
-variable "AWS_ACCESS_KEY" {
-}
 
-variable "AWS_SECRET_KEY" {
-}
 
 variable "AWS_REGION" {
-  default = "us-east-2"
+  default = "us-east-1"
 }
 
 variable "AMIS" {
   type = map(string)
   default = {
-    us-east-2 = "ami-0fa49cc9dc8d62c84"
+    us-east-1 = "ami-0022f774911c1d690"
   }
 }
